a4d9b3f4dd6fc10173c9d0141f557c3bfeff5a89
Ellian Aragão Dias <ellian.aragao@gmail.com>
2023-12-03 17:29:51 -0300

    feat: init adjacent list impl and using abstract class

--- trabalho1-kotlin/src/main/kotlin/aragao/ellian/github/graph/entity/AdjacentList.kt
+++ trabalho1-kotlin/src/main/kotlin/aragao/ellian/github/graph/entity/AdjacentList.kt
@@ -1,4 +1,18 @@
 package aragao.ellian.github.graph.entity
 
-class AdjacentList(private val totalVertex: Int) {
+import java.util.*
+
+class AdjacentList(private val totalVertex: Int) : GraphAbstract(totalVertex) {
+    private val adjacentList = Array(totalVertex) { LinkedList<Int>() }
+
+    override fun insertEdge(vertex1: Int, vertex2: Int) {
+        adjacentList[vertex1].add(vertex2)
+    }
+
+    override fun processVertexDegree(): Array<Int> {
+        return adjacentList
+            .map { it.size / 2 }
+            .toList()
+            .toTypedArray()
+    }
 }
